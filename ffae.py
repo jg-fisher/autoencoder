669241a6741ae2a11f9a8c805adcdb8d784d5dc6
John Fisher <jfishersolutions@gmail.com>
2018-07-19 10:45:17 -0400

    refactor and model saves

--- ffae.py
+++ ffae.py
@@ -3,25 +3,65 @@ from keras.layers import Input, Dense
 from keras.models import Model
 from keras.callbacks import TensorBoard
 import numpy as np 
+from tensorflow import set_random_seed
+import os
 
+class AutoEncoder:
+    def __init__(self, encoding_dim=3):
+        self.encoding_dim = encoding_dim
+        self.x = np.array([[1,2,3] for _ in range(10)])
 
-# this is the size of our encoded representations
-encoding_dim = 3
+    def seedy(self, s):
+        np.random.seed(s)
+        set_random_seed(s)
+    
+    def _encoder(self):
+        inputs = Input(shape=(self.x[0].shape))
+        encoded = Dense(self.encoding_dim, activation='relu')(inputs)
+        model = Model(inputs, encoded)
+        self.encoder = model
+        return model
 
-x = np.array([[1,2,3] for _ in range(10)])
+    def _decoder(self):
+        inputs = Input(shape=(self.encoding_dim,))
+        decoded = Dense(3)(inputs)
+        model = Model(inputs, decoded)
+        self.decoder = model
+        return model
 
-inputs = Input(shape=(3,))
-encoded = Dense(encoding_dim, activation='relu')(inputs)
-decoded = Dense(3)(encoded)
+    def encoder_decoder(self):
+        ec = self._encoder()
+        dc = self._decoder()
+        
+        inputs = Input(shape=self.x[0].shape)
+        ec_out = ec(inputs)
+        dc_out = dc(ec_out)
+        model = Model(inputs, dc_out)
+        
+        self.model = model
+        return model
 
-# this model maps an input to its reconstruction
-autoencoder = Model(inputs, decoded)
-autoencoder.compile(optimizer='adadelta', loss='mse')
+    def fit(self, epochs=300):
+        self.model.compile(optimizer='adadelta', loss='mse')
+        log_dir = './tmp'
+        tbCallBack = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
+        self.model.fit(self.x, self.x,
+                        epochs=epochs,
+                        batch_size=4,
+                        callbacks=[tbCallBack])
 
-log_dir = './tmp'
-tbCallBack = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=0, write_graph=True, write_images=True)
+    def save_weights(self):
+        if not os.path.exists(r'./weights'):
+            os.mkdir(r'./weights')
+        else:
+            self.encoder.save(r'./weights/encoder_weights.h5')
+            self.decoder.save(r'./weights/decoder_weights.h5')
+            self.model.save(r'./weights/ae_weights.h5')
+        
 
-autoencoder.fit(x, x,
-                epochs=500,
-                batch_size=4,
-                callbacks=[tbCallBack])
+if __name__ == '__main__':
+    ae = AutoEncoder(encoding_dim=2)
+    ae.seedy(3)
+    ae.encoder_decoder()
+    ae.fit()
+    ae.save_weights()
